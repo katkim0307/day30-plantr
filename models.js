3dd41d81e0a0f839567c097ee9d74d4a2a646e1e
Kat Kim <56666457+katkim0307@users.noreply.github.com>
2020-07-22 13:54:00 -0400

    CONNECTED (db.sync) to plantr db without using async/await, BUILT Gardner, Plot, Vegetable models

--- models.js
+++ models.js
@@ -4,4 +4,52 @@ const db = new Sequelize('plantr', 'postgres', 'jh810506', {
     logging: false,
 });
 
-module.exports = db;
+const Gardener = db.define('gardener', {
+    name: {
+        type: Sequelize.STRING,
+        allowNull: false,
+    },
+    age: {
+        type: Sequelize.INTEGER,
+        allowNull: false,
+    },
+});
+
+const Plot = db.define('plot', {
+    size: {
+        type: Sequelize.INTEGER,
+        allowNull: false,
+    },
+    shaded: {
+        type: Sequelize.BOOLEAN,
+        allowNull: false,
+    },
+});
+
+const Vegetable = db.define('vegetable', {
+    name: {
+        type: Sequelize.STRING,
+        allowNull: false,
+    },
+    color: {
+        type: Sequelize.STRING,
+        allowNull: false,
+    },
+    planted_on: {
+        type: Sequelize.DATEONLY,
+        get() {
+            return moment(this.getDataValue('planted_on')).format('YYYY/MM/DD');
+        },
+        allowNull: false,
+    },
+});
+
+// SEQUELIZE ASSOCIATIONS
+Plot.belongsTo(Gardener);
+Gardener.hasOne(Plot);
+Gardener.belongsTo(Vegetable, { as: 'fav_vegetable' });
+// MANY-MANY ASSOCIATION W/ FOREIGN KEY
+Vegetable.belongsToMany(Plot, { through: 'vegetable_plot' })
+Plot.belongsToMany(Vegetable, { through: 'vegetable_plot' });
+
+module.exports = { db, Gardener, Plot, Vegetable };
